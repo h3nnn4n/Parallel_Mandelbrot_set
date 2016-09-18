72521e763c601af59f771974f34943d9b616c560
h3nnn4n <uber.renan@gmail.com>
2016-09-18 12:09:13 +0000

    Added Floyd's cycle checking

--- mandelbrot.c
+++ mandelbrot.c
@@ -2,26 +2,42 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
-#include "color.h"
+
+typedef struct{
+        double r, g, b;
+}_color;
 
 int process_point(double cx, double cy, int er, int bailout) {
-    int i;
-    double zx, zxn, zy, zyn;
+    int    i;
+    double zx , zxn , zy , zyn;
+    double zx2, zxn2, zy2, zyn2;
 
-    zx = 0.0;
-    zy = 0.0;
+    zx  = 0.0;
+    zy  = 0.0;
+    zx2 = 0.0;
+    zy2 = 0.0;
 
     for ( i = 0; i < bailout; i++ ){
         zxn = zx * zx - zy * zy + cx;
         zyn = 2.0 * zx * zy + cy;
-        zx  = zxn;
-        zy  = zyn;
+        zx  = zxn; zy  = zyn;
+
+        zxn = zx * zx - zy * zy + cx;
+        zyn = 2.0 * zx * zy + cy;
+        zx  = zxn; zy  = zyn;
+
+        zxn2 = zx2 * zx2 - zy2 * zy2 + cx;
+        zyn2 = 2.0 * zx2 * zy2 + cy;
+        zx2  = zxn2; zy2  = zyn2;
+
+        if ( zx2 == zx && zy2 == zy )
+            return 0;
 
         if ( zx * zx + zy * zy > er * er )
             return i;
     }
 
-    return 0.0;
+    return 0;
 }
 
 int main(){
@@ -45,7 +61,7 @@ int main(){
     minx     =  -2.5;
     maxx     =   1.5;
     miny     =  -2.0;
-    maxy     =  miny*-1;
+    maxy     =  miny * -1;
 
     printf("%f \t %f\n%f\t %f\n", minx, maxx, miny, maxy);
 
@@ -54,12 +70,12 @@ int main(){
 
     for(iy = 0; iy < screeny; iy++ ){
         cy = miny+iy*(maxy-miny)/screeny;
-        for(ix = 0;ix<screenx;ix++){
-            cx = minx+ix*(maxx-minx)/screenx;
+        for ( ix = 0; ix < screenx; ix++ ) {
+            cx = minx + ix * (maxx - minx) / screenx;
             escapetime[iy * screenx + ix] = process_point(cx, cy, er, bailout);
         }
 
-        if(iy%(screeny/10) == 0){
+        if ( iy % ( screeny / 10 ) == 0 ) {
             fprintf(stderr," -- %.2f%%\n",(iy/(double)screeny)*100.0);
         }
     }
